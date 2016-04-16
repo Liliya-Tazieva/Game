eda39700aba2a38263726a791ca3f83b5ee4a9fa
Liliya <tazieva2107@gmail.com>
2016-04-16 06:07:00 +0300

    TilesManager, MapManager
    Can generate a map from image, that contain 2 colors: black and white

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapManager : MonoBehaviour {
+
+    public TextAsset MapImg;
+    public TilesManager TilesM;
+
+	// Use this for initialization
+	void Start () {
+        Texture2D map = new Texture2D(2, 2);
+	    if (!map.LoadImage(MapImg.bytes)) Debug.Log("Can't load image");
+	    else {
+	        for(int i = 0; i<map.height; ++i)
+	            for (int j = 0; j < map.width; ++j) {
+	                Color color = map.GetPixel(i, j);
+                    GameObject prefab = TilesM.GetPrefab(color);
+                    Vector3 position = new Vector3(i * 3.0f, 0.0f, j * 3.0f);
+                    Instantiate(prefab, position, Quaternion.identity);
+	            }
+	    }
+	}
+	
+}
---
+++ Assets/Scripts/TilesManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TilesManager: MonoBehaviour {
+
+    public List<GameObject> Prefabs;
+    public List<Color> Colors;
+
+    void Start() {
+        Debug.Log("Initialize TilesManager");
+    }
+
+    public GameObject GetPrefab(Color color) {
+        int index = Colors.IndexOf(color);
+        return Prefabs[index];
+    }
+}
